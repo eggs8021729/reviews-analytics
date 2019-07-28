74bfb13df31ae58c730c984585b8438e55d7b7ba
Allen Lin <eggs8021729@gmail.com>
2019-07-28 18:23:49 +0800

    calc count read.py

--- read.py
+++ read.py
@@ -1,4 +1,4 @@
-data = []
+data = [] #空清單
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
@@ -6,9 +6,11 @@ with open('reviews.txt', 'r') as f:
 		count = count + 1 # 意思同count += 1
 		if count % 1000 == 0: # %求餘數 除1000餘數=0
 			print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 
-print(data[0])
-print('-----------------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度為', sum_len/len(data))
